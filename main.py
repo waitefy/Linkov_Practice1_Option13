da34da16aadd6a046379716053e9d690f610dcd8
waitefy <matvey23lin@gmail.com>
2025-09-20 14:14:53 +0300

    fix (Этап 1. REPL): exit завершает программу даже при переданных аргументах

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import os
 import socket
+import sys
 
-username = os.getlogin()
-hostname = socket.gethostname()
-line = input(f'{username}@{hostname}:~$ ')
 
-while line != 'exit':
+def repl(username, hostname):
+    while True:
+        line = input(f'{username}@{hostname}:~$ ')
+        execute_command(line, username, hostname)
+
+
+def execute_command(line, username, hostname):
+    if not line.strip():
+        return
     parts = line.split()
     command, args = parts[0], parts[1:]
 
     if command in ('ls', 'cd'):
         print(line)
+    elif command == 'exit':
+        sys.exit(0)
     else:
         print(f'{command}: команда не найдена')
 
-    line = input(f'{username}@{hostname}:~$ ')
+
+def main():
+    username = os.getlogin()
+    hostname = socket.gethostname()
+    repl(username, hostname)
+
+
+if __name__ == '__main__':
+    main()
